4ec77ef543c50c795aea588dbf51df3ee0573165
SciKa <enzoescipy@gmail.com>
2021-12-26 14:16:39 +0900

    local:simp correc.

--- server_express/main.js
+++ server_express/main.js
@@ -1,60 +1,64 @@
-//express
-const express = require('express')
+await function main()
+{  //express
+  const express = require('express')
 
-//middle-wares
-var bodyParser = require('body-parser')
-var helmet = require('helmet')
-var csp = require('helmet-csp')
-var compression = require('compression')
-var path = require('path')
+  //middle-wares
+  var bodyParser = require('body-parser')
+  var helmet = require('helmet')
+  var csp = require('helmet-csp')
+  var compression = require('compression')
+  var path = require('path')
 
-//project routers
-//var indexRouter = require('./routes/index')
-var crudRouter = require('./routes/router')
+  //project routers
+  //var indexRouter = require('./routes/index')
+  var crudRouter = require('./routes/router')
 
-//var fs = require('fs')
-const port = (8080)
-const app = express()
+  //var fs = require('fs')
+  const port = (8080)
+  const app = express()
 
-//views engine setting
-app.set('views', './views');
-app.set('view engine', 'pug');
+  //views engine setting
+  app.set('views', './views');
+  app.set('view engine', 'pug');
 
-//middlewares use
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(compression())
-app.use(helmet())
-app.use(
-    csp({
-      directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'"],
-        scriptSrc: ["'self' 'unsafe-inline'"],
-      },
-    })
-  )
+  //middlewares use
+  app.use(bodyParser.urlencoded({extended: false}))
+  app.use(compression())
+  app.use(helmet())
+  app.use(
+      csp({
+        directives: {
+          defaultSrc: ["'self'"],
+          styleSrc: ["'self'"],
+          scriptSrc: ["'self' 'unsafe-inline'"],
+        },
+      })
+    )
 
-//routers url and express static.
-app.use(express.static('public'));
-app.use('/node_modules',express.static(path.join(__dirname, '/node_modules')))
-app.use('/',crudRouter)
+  //routers url and express static.
+  app.use(express.static('public'));
+  app.use('/node_modules',express.static(path.join(__dirname, '/node_modules')))
+  app.use('/',crudRouter)
 
-//custom-wares
-var Mongo = require("./module/mongodb-communicate/server_mongod")
-await Mongo.initialize()
-await Mongo.update()
-await Mongo.debug()
+  //custom-wares
+  var Mongo = require("./module/mongodb-communicate/server_mongod")
+  await Mongo.initialize()
+  await Mongo.update()
+  await Mongo.debug()
 
 
-//404 error case
-app.use(function (req, res, next){
-    res.status(404).send('cannot find any objects...')
-})
+  //404 error case
+  app.use(function (req, res, next){
+      res.status(404).send('cannot find any objects...')
+  })
 
-//server error case
-app.use(function(err, req, res, next) {
-    console.error(err,stack)
-    res.status(500).send("something wrong...")
-})
+  //server error case
+  app.use(function(err, req, res, next) {
+      console.error(err,stack)
+      res.status(500).send("something wrong...")
+  })
 
-app.listen(port, () => console.log("app launched..."))
+  app.listen(port, () => console.log("app launched..."))
+}
+
+main()
