1077708db4d4ea349cfab79c2cb2b3c258ff69ef
SciKa <enzoescipy@gmail.com>
2022-01-06 16:58:18 +0900

    local:simp correc.

--- server_express/module/mongoCalc/mongoCalc.py
+++ server_express/module/mongoCalc/mongoCalc.py
@@ -2,4 +2,7 @@ from pymongo import MongoClient
 
 client = MongoClient(host='localhost', port=27017)
 
-print(client.Notionpage_workid.todo.find())
+cursor = client.Notionpage_workid.todo.find()
+
+for doc in cursor:
+    print(doc)
