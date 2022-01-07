8da46d5ac8a69f57800c8445a472a669755f2330
SciKa <enzoescipy@gmail.com>
2022-01-08 00:19:21 +0900

    local:simp correc.

--- server_express/routes/router.js
+++ server_express/routes/router.js
@@ -44,6 +44,7 @@ router.post('/api/pythonCalc/test', function(req, res) {
 //python test result router
 router.post('/result/test', function(req, res) {
     Mongo_py.find_test((finded) => {
+        console.log(finded)
         res.render('pythonresult_test',{calculated : finded[value]})
     })
 })
