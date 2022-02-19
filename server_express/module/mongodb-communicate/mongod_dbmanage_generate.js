b7cd46e2497abacbc26503b416da20731af65050
SciKa <enzoescipy@gmail.com>
2022-02-19 22:37:32 +0900

    simp correction.

--- server_express/module/mongodb-communicate/mongod_dbmanage_generate.js
+++ server_express/module/mongodb-communicate/mongod_dbmanage_generate.js
@@ -5,7 +5,6 @@ const uri = "mongodb://localhost:27017"
 
 const client = new MongoClient(uri);
 
-const dbnaming = require('./mongod_dbmanage_Name')
 
 async function insertRandomDatepairs(dbNamenum, dbTypenum, collectionTypenum,datestring, callback)
 {
