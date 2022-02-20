a279b6af4ee5197ee38689ba42fb3ff59d46f42b
SciKa <enzoescipy@gmail.com>
2022-02-21 01:04:56 +0900

    simp correction.

--- server_express/module/mongodb-communicate/mongod_dbmanage_public.js
+++ server_express/module/mongodb-communicate/mongod_dbmanage_public.js
@@ -1,2 +1,37 @@
-const mongoNaming = require('./mongod_dbmanage_Name')
 const mongoGenerate = require('./mongod_dbmanage_generate')
+
+
+
+async function makeNewDBset(DB_namenum, collection_namenum)
+{
+    for (var i=0; i<3;i++)
+    {
+        await mongoGenerate.makeNewDB(DB_namenum,i,collection_namenum)
+    }
+}
+
+async function reloadDB_main(DB_namenum, collection_namenum)
+{
+    await mongoGenerate.update(DB_namenum,1,collection_namenum)
+}
+
+async function generateDB_test(DB_namenum, collection_namenum)
+{
+    await mongoGenerate.insertRandomDatepairs(DB_namenum,0,collection_namenum)
+}
+
+async function saveDB_main(DB_namenum, collection_namenum)
+{
+    await mongoGenerate.copypaste(DB_namenum,1,collection_namenum, DB_namenum,2,collection_namenum)
+}
+
+async function takeDBfromBackUp_test(DB_namenum, collection_namenum)
+{
+    await mongoGenerate.copypaste(DB_namenum,2,collection_namenum, DB_namenum,0,collection_namenum)
+}
+
+exports.makeNewDBset = makeNewDBset
+exports.reloadDB_main = reloadDB_main
+exports.generateDB_test = generateDB_test
+exports.saveDB_main = saveDB_main
+exports.takeDBfromBackUp_test = takeDBfromBackUp_test
