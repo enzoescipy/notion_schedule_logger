d15fb90e451ca900661e944e30478a2954451008
SciKa <enzoescipy@gmail.com>
2022-03-15 00:52:16 +0900

    simp correction.

--- server_express/module/mongoCalc/main.py
+++ server_express/module/mongoCalc/main.py
@@ -5,15 +5,15 @@ from datetime import date
 
 client = MongoClient(host='localhost', port=27017)
 
-cursor = client.Notionpage_workid.todo.find()
-
-length = 123
-
-resultDB, resultCollection = getName(0,3,0)
-resultDB = client[resultDB]
-resultCollection = resultDB[resultCollection]
-
-def post_setRateOfProp(propname, rate):
+def post_setRateOfProp(propname, rate, isTest):
+    if isTest == True:
+        isTest = 0
+    elif isTest == False:
+        isTest = 1
+    else:
+        isTest = "invalid"
+    selected_name = getName(0,1,isTest,0)
+    print(selected_name)
     # make rate resonable. not int -> to int, over range -> boundary set.
     if rate <= 1 :
         rate = 1
@@ -26,15 +26,4 @@ def post_setRateOfProp(propname, rate):
     todaystring = date.today().isoformat()
 
     #make dataset
-    today_data = {}
-
-    #check if there is same propname in collection.
-    docSet = resultCollection.find({"id":propname},{})
-    if docSet == None :
-        return -1
-
-    resultCollection.replace_one({"id":propname},{"id":propname, "rate": rate},upsert=True)
-    print("python : task fin.")
-    sys.stdout.flush()  
-
 
