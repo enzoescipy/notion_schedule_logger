a5d8bd9df250467e797ebfe909633f0059bfda0d
SciKa <enzoescipy@gmail.com>
2022-03-17 14:34:32 +0900

    simp correction.

--- server_express/module/mongoCalc/test.py
+++ server_express/module/mongoCalc/test.py
@@ -2,12 +2,12 @@ import sys
 
 print("hihihihihi")
 sys.stdout.flush()
-'''
+
 fget = sys.argv[1]
-fvar = sys.argv.copy().pop(0)
+fvar = sys.argv.copy()
+fvar.pop(0)
 fvar.pop(0)
 
 if fget == 0:
     print(fvar ,"hello?")
     sys.stdout.flush()
-'''
