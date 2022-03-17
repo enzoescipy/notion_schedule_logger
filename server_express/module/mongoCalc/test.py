17775583e168d3bfc5cdc6ac40739b3dbce1d13f
SciKa <enzoescipy@gmail.com>
2022-03-17 15:03:03 +0900

    simp correction.

--- server_express/module/mongoCalc/test.py
+++ server_express/module/mongoCalc/test.py
@@ -1,10 +1,14 @@
 import sys
-import copy
 
 fget = sys.argv[1]
 fvar = sys.argv.copy()
 fvar.pop(0)
 fvar.pop(0)
 
-print(fvar,"hello?")
-sys.stdout.flush()
+
+if fget == "0":
+    print(fget, fvar, "hello?")
+    sys.stdout.flush()
+else:
+    print("invalid input.")
+    sys.stdout.flush()
