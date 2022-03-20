5c3d0d55e7594d9687c0b19a7984b47256be06db
SciKa <enzoescipy@gmail.com>
2022-03-20 18:58:45 +0900

    simp correction.

---
+++ server_express/views/index.js
@@ -0,0 +1,20 @@
+document.write("hello, world!")
+
+/*
+var sliders = document.querySelectorAll('input[type="range"].slider')
+sliders.forEach.call((slider) => {
+  var id = slider.id 
+  var outputsSet = document.getElementByTagName('output')
+  for (i in outputsSet)
+  {
+    var output = outputSet[i]
+    if (output.htmlFor == idVal)
+    {
+      //- connect the output and slider.
+      slider.addEventListener('input', (event) => {
+        output.value = event.target.value
+      })
+    }
+  }
+}) 
+*/
