bbb43a8bd300fba7f0fa59d841dfb7974f23823a
SciKa <enzoescipy@gmail.com>
2022-03-21 20:08:55 +0900

    simp correction.

--- server_express/module/notion-communicate/index.js
+++ server_express/module/notion-communicate/index.js
@@ -97,7 +97,7 @@ async function getItem_seletDate(databaseId, datestring) // YYYY-MM-DD
   try {
     //prepare to change day of week to date
     var currnet_date = moment(datestring,"YYYY-MM-DD")
-    console.log(toString(currnet_date))
+    console.log(toString(currnet_date) === "[object Undefined]")
     //currnet_date.subtract(3,"days")//--debug
     var current_day = (Number(currnet_date.format("d")) +6 ) % 7
     if (current_day == 0) 
