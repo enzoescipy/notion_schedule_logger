83509889dfbeec68990ac991f2e1bb414a622f70
SciKa <enzoescipy@gmail.com>
2022-03-24 21:28:37 +0900

    simp correction.

--- server_express/module/mongodb-frontend/mongod_dbtakeout_calculation.js
+++ server_express/module/mongodb-frontend/mongod_dbtakeout_calculation.js
@@ -101,7 +101,7 @@ async function calc_pointer_reOrganize(dbNamenum, dbTypenum, collectionTypenum, 
     }
     function colorsampler(colorized)
     {
-        var sample = ["white","black","light","dark","primary","link","info","succes","warning","danger"]
+        var sample = ["dark","primary","link","info","succes","warning","danger"]
         for (j in sample)
         {
             var suggested = sample[j]
