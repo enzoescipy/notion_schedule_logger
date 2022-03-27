614beab25a2c3d41d66105382bbf1419b1ea650e
SciKa <enzoescipy@gmail.com>
2022-03-27 14:54:19 +0900

    simp correction.

--- server_express/DBtimer.js
+++ server_express/DBtimer.js
@@ -29,9 +29,10 @@ async function init()
     {
         var DBnaming = preset_DBnaming[i]
         //mongo init
-        await publicMongo.reloadDB_main(DBnaming[0],DBnaming[1],DBnaming[2])
         await settingMongo.set(1,1,true,DBnaming[0],DBnaming[1],DBnaming[2])
         await settingMongo.set(0,0,7,DBnaming[0],DBnaming[1],DBnaming[2])
+        await publicMongo.reloadDB_main(DBnaming[0],DBnaming[1],DBnaming[2])
+
         //python init
         var pythonprocess_1 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 1,DBnaming[0],DBnaming[2],DBnaming[1],proprate,ignorance])
         var pythonprocess_2 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 2,DBnaming[0],DBnaming[2],DBnaming[1]])
