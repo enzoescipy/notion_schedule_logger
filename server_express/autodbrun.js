fc73d6c022bcb0582e9c009c32b5552b03751a31
SciKa <enzoescipy@gmail.com>
2022-03-27 23:05:10 +0900

    simp correction.

--- server_express/autodbrun.js
+++ server_express/autodbrun.js
@@ -5,7 +5,7 @@ var currentmode = istest.NOWNUM()
 
 currentmode.then((val) => {
 	dbtimer.preset_addDB(0,Number(val),0)
-	dbtimer.preset_addRepeatTime(60)
+	dbtimer.preset_addRepeatTime(600)
 	dbtimer.preset_initial_ratesetting(25, 1)
 
 	dbtimer.START()
