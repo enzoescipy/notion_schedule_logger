3ed73f8eda1aeff07deb120e48f29da7c5b9d70a
SciKa <enzoescipy@gmail.com>
2022-03-28 06:12:06 +0900

    simp correction.

--- server_express/module/mongoCalc/main.py
+++ server_express/module/mongoCalc/main.py
@@ -131,7 +131,7 @@ def post_setRateOfProp_noflush(dbname, dbcollec,propname, rate, fromTest,ignoran
             docs[todaystring]["ignorance"] = ignorance
         else:
             docs[todaystring] = {"ignorance":ignorance,"rate_abs" : rate, "rate_rel" : "invalid"}
-        print(docs)
+        del(docs["_id"])
         collec.replace_one({"id" : propname,"sub-collec":"rater"}, docs, upsert=True)
 
     # put and calculate the rate_rel
