08777fb761a0a10adefa494358ef382880eb4eca
SciKa <enzoescipy@gmail.com>
2022-03-27 15:11:31 +0900

    simp correction.

--- server_express/DBtimer.js
+++ server_express/DBtimer.js
@@ -16,19 +16,22 @@ async function preset_addDB(dbNamenum, dbTypenum, collectionTypenum)
 {
     preset_DBnaming.push([dbNamenum, dbTypenum, collectionTypenum])
     ispreset_addDB_raised = true
+    return preset_DBnaming
 }
 var ispreset_repeatTime_raised = false
 async function preset_addRepeatTime(sec)
 {
-    preset_repeat_ms = sec * 1000
+    preset_repeat_ms = Number(sec) * 1000
     ispreset_repeatTime_raised = true
+    return preset_repeat_ms
 }
 var ispreset_initial_ratesetting_raised = false
 async function preset_initial_ratesetting(rate, igno)
 {
-    initrate = rate
-    initignorance = igno
+    initrate = Number(rate)
+    initignorance = Number(igno)
     ispreset_initial_ratesetting_raised = true
+    return [initrate,initignorance]
 }
 async function init()
 {
@@ -36,8 +39,10 @@ async function init()
     {
         var DBnaming = preset_DBnaming[i]
         //mongo init
+        console.log("settingDB...")
         await settingMongo.set(1,1,true,DBnaming[0],DBnaming[1],DBnaming[2])
         await settingMongo.set(0,0,7,DBnaming[0],DBnaming[1],DBnaming[2])
+        console.log("notionMainDB...")
         await publicMongo.reloadDB_main(DBnaming[0],DBnaming[1],DBnaming[2])
 
         //python init
@@ -45,13 +50,17 @@ async function init()
         var pythonprocess_2 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 2,DBnaming[0],DBnaming[2],DBnaming[1]])
         var pythonprocess_4 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 4,DBnaming[0],DBnaming[2],DBnaming[1]])
 
+        console.log("pyProprateOptimize...")
+
         pythonprocess_1.stdout.on('data', chain1)
         function chain1(data)
         {
+            console.log("pyCalculaion...")
             pythonprocess_2.stdout.on('data', chain2)
         }
         function chain2(data)
         {
+            console.log("pycmmulative...")
             pythonprocess_4.stdout.on('data',()=>{} )
         }
 
@@ -63,8 +72,10 @@ async function repeat()
     for (let i=0; i<preset_DBnaming.length; i++)
     {
         var DBnaming = preset_DBnaming[i]
+        console.log("notionMainDB...")
         await publicMongo.reloadDB_main(DBnaming[0],DBnaming[1],DBnaming[2])
         var pythonprocess_3 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py",3,DBnaming[0],DBnaming[2],todaystring,DBnaming[1]])
+        console.log("pyCalculation_small...")
         pythonprocess_3.stdout.on('data', () => {})
     }
 
