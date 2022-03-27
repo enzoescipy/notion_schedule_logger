8e359e856c4d6a430d4a28dc0d6351a3366efd90
SciKa <enzoescipy@gmail.com>
2022-03-28 04:08:19 +0900

    simp correction.

--- server_express/module/mongoCalc/main.py
+++ server_express/module/mongoCalc/main.py
@@ -532,7 +532,7 @@ def post_faultRateEliminate(dbname, dbcollec,fromTest, rate, ignorance):
     dbname = int(dbname)
     dbcollec = int(dbcollec)
     fromTest = int(fromTest)
-    today = date.today()
+    today = date.fromisoformat(date.today().isoformat())
 
     selected_name = getName(dbname,0,fromTest,dbcollec)
     client = MongoClient(host='localhost', port=27017)
@@ -561,7 +561,7 @@ def post_faultRateEliminate(dbname, dbcollec,fromTest, rate, ignorance):
             return date.fromisoformat(target[0])
         doc_ordered.sort(key=sorter)
         newest_date_str = doc_ordered[-1][0]
-        writeLog(newest_date_str, doc_ordered, today.isoformat())
+        writeLog(doc["id"], newest_date_str, doc_ordered, today.isoformat())
         newest_date = date.fromisoformat(newest_date_str)
 
         if newest_date < today:
