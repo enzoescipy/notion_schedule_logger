327b4025995a0e3a08ff9359465110f055353d7d
SciKa <enzoescipy@gmail.com>
2022-03-27 12:58:23 +0900

    simp correction.

--- server_express/module/mongoCalc/main.py
+++ server_express/module/mongoCalc/main.py
@@ -485,6 +485,7 @@ def calc_gPP_updateOne(propdate, fromTest):
         propday = propdate_countable.weekday()
         for i in range(propday+1):
             print(i)
+            sys.stdout.flush()
             propdate_now = propdate_countable.isoformat()
             doc[propdate_now] = calc_getPointOfProp_noflush(doc["id"], propdate_now, fromTest)
             propdate_countable -= timedelta(days=1)
@@ -590,6 +591,7 @@ def calc_sCO_updateOne(propdate, fromTest):
 
         for i in range(propday+1):
             print(i)
+            sys.stdout.flush()
             propdate_before_str = propdate_before.isoformat()
             propdate_now_str = propdate_now.isoformat()
             commulative_before = doc[propdate_before_str]
