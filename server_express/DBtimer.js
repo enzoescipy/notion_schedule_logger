c4dcb877922074a39f26377d332d01958d4d2a21
SciKa <enzoescipy@gmail.com>
2022-03-27 19:02:19 +0900

    simp correction.

--- server_express/DBtimer.js
+++ server_express/DBtimer.js
@@ -74,21 +74,20 @@ async function init(callback)
 
         //python init
         var pythonprocess_1 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 1,DBnaming[0],DBnaming[2],DBnaming[1],initrate,initignorance])
-        var pythonprocess_2 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 2,DBnaming[0],DBnaming[2],DBnaming[1]])
-        var pythonprocess_4 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 4,DBnaming[0],DBnaming[2],DBnaming[1]])
-
         console.log("pyProprateOptimize...")
         pythonprocess_1.stdout.on('data', chain1)
         function chain1(data)
         {
             console.log(data.toString())
             console.log("pyCalculaion...")
+            var pythonprocess_2 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 2,DBnaming[0],DBnaming[2],DBnaming[1]])
             pythonprocess_2.stdout.on('data', chain2)
         }
         function chain2(data)
         {
             console.log(data.toString())
             console.log("pycmmulative...")
+            var pythonprocess_4 = spawn('./python3-server/bin/python', ["./module/mongoCalc/main.py", 4,DBnaming[0],DBnaming[2],DBnaming[1]])
             pythonprocess_4.stdout.on('data',chain3 )
         }
         function chain3(data)
