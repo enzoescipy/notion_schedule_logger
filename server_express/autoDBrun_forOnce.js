9ed23df91987d59c1af7892eb3461456c21695cf
SciKa <enzoescipy@gmail.com>
2022-03-28 15:30:15 +0900

    simp correction.

--- server_express/autoDBrun_forOnce.js
+++ server_express/autoDBrun_forOnce.js
@@ -9,6 +9,7 @@ currentmode.then(start)
 async function start(val)
 {
 	await dbtimer.preset_addDB(0,Number(val),0)
+	await dbtimer.preset_addDB(0,Number(val),1)
 	await dbtimer.preset_addRepeatTime(20)
 	await dbtimer.preset_initial_ratesetting(25, 1)
 
