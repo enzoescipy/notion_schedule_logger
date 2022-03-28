564df376a688cd3ef39eac982424bf7202ada1f5
SciKa <enzoescipy@gmail.com>
2022-03-28 16:50:15 +0900

    simp correction.

--- server_express/autoDBrun.js
+++ server_express/autoDBrun.js
@@ -9,7 +9,10 @@ currentmode.then(start)
 async function start(val)
 {
 	await dbtimer.preset_addDB(0,Number(val),0)
-	await dbtimer.preset_addRepeatTime(20)
+	await dbtimer.preset_addDB(0,Number(val),1)
+	await dbtimer.preset_addDB(0,Number(val),2)
+	
+	await dbtimer.preset_addRepeatTime(600)
 	await dbtimer.preset_initial_ratesetting(25, 1)
 
 	await dbtimer.START()
